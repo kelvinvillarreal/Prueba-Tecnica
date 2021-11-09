1dd9d315100c186700b7e70013cfc0a87171b062
Kelvin Villarreal Godoy <69981840+kelvinvillarreal@users.noreply.github.com>
2021-11-09 23:07:58 +0100

    Delete PruebaTecnicaReactiveSpringBootWebFlux directory

--- PruebaTecnicaReactiveSpringBootWebFlux/reactiveStream/spring-boot-webflux/src/main/java/com/vic/webflux/app/models/dao/ICategoriaDao.java
+++
@@ -1,9 +0,0 @@
-package com.vic.webflux.app.models.dao;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.vic.webflux.app.models.documents.Categoria;
-
-public interface ICategoriaDao extends ReactiveMongoRepository<Categoria, String> {
-
-}
